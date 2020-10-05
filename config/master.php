4c39e5a8b1c20c24daa896f505951ac31a84dfc3
Richard Gomer <richard@richardgomer.co.uk>
2020-10-05 19:57:05 +0100

    ...

--- config/master.php
+++ config/master.php
@@ -92,7 +92,7 @@ $sd = new Schedule\DailyScheduler('light.scheduler', 'global.schedules', 'daylig
 $conf['devices'][] = $sd;
 
 // Create a socket to be controlled and bind it to the schedule in the broker
-$client = new MQTT\Client('10.0.0.8', 1883);
+$client = new MQTT\Client($mqtthost, 1883);
 $conf['devices'][] = $socket = new Light\RGBWCT("light1", $client, "light1", 'global.schedules', 'daylightschedule');
 $conf['devices'][] = $socket = new Light\RGBWCT("light2", $client, "light1", 'global.schedules', 'daylightschedule');
 $conf['devices'][] = $socket = new Light\RGBWCT("light3", $client, "light1", 'global.schedules', 'daylightschedule');
