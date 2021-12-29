ae1eb9a025583ab74728ba57022c58de65a01995
Richard Gomer <r.gomer@soton.ac.uk>
2021-12-29 14:14:16 +0000

    Start sketching multithread support

--- bin/run.php
+++ bin/run.php
@@ -38,57 +38,88 @@ if(!$args['config']) {
     }
 }
 
-// Record runtime status information
-$status = new Storage\JsonStore('devicestatus');
-$status->proc = getmypid();
-$status->started = time();
-$status->ips = $ips = System\IP::getIPs();
+/**
+ * TODO: Allow multiple brokers to be started; each in a thread.
+ * If $conf[] is just an integer-indexed array, then treat each key as a
+ * separate config array. For each one:
+ * Set a config number; spawn a new thread; have the child thread kick off the
+ * broker using the configured parameters.
+ * The main thread will just wait until the others are done.
+ * Each thread will have its own remote delivery device etc.
+ *
+ * Options: have threads share the incoming queue but only pick up their own
+ * commands? Or use the main thread to split them up somehow? JSONQueue would
+ * need to be device-aware to do filtering; otherwise peek/shift don't make sense
+ * COULD: Use the main thread as a router; and have the child threads do all routing
+ * through it (i.e they have only a default endpoint, receive no announcements).
+ * But take their announcements into our own device map (that would happen automatically
+ * anyway; as they announce to us) and do routing via a new kind of endpoint - shmop?, jsonqueue?
+ *
+ * Check: Each thread will do its own announcements, but to a single HTTP
+ * endpoint. So check that that works (and that they don't e.g. overwrite
+ * one anothers announcements)
+ *
+ *
+ */
 
-$inputQueue = $conf['queue_in'];
-$remoteQueue = $conf['queue_remote'];
-$deviceMap = $conf['device_map'];
+/**
+ * Start a single broker using the defined config in $conf
+ */
+function startBroker($conf) {
+  // Record runtime status information
+  $status = new Storage\JsonStore('devicestatus');
+  $status->proc = getmypid();
+  $status->started = time();
+  $status->ips = $ips = System\IP::getIPs();
+
+  $inputQueue = $conf['queue_in'];
+  $remoteQueue = $conf['queue_remote'];
+  $deviceMap = $conf['device_map'];
 
 
-$broker->setRemoteQueue($remoteQueue);
-$broker->setInputQueue($inputQueue);
+  $broker->setRemoteQueue($remoteQueue);
+  $broker->setInputQueue($inputQueue);
 
-// The announcement device tells other endpoints about our local devices
-// so that they can route commands to them
-$announce = new Device\AnnouncerDevice($conf['endpoint_url'], array(), $deviceMap);
-$broker->addDevice($announce);
+  // The announcement device tells other endpoints about our local devices
+  // so that they can route commands to them
+  $announce = new Device\AnnouncerDevice($conf['endpoint_url'], array(), $deviceMap);
+  $broker->addDevice($announce);
 
-// If there's a default endpoint configured, we'll announce to it
-if($conf['default_endpoint'] !== false) {
-    $announce->addRemote($conf['default_endpoint']);
+  // If there's a default endpoint configured, we'll announce to it
+  if($conf['default_endpoint'] !== false) {
+      $announce->addRemote($conf['default_endpoint']);
+  }
+
+  // ** DISABLE DIRECT LOCAL DELIVERY **
+  // Direct local delivery can be disabled, in which case the message broker
+  // will push all messages via the remote queue. We need to announce to ourself
+  // so that those messages can be routed into our own endpoint.
+  if($conf['disable-direct-local']) {
+      $broker->disableDirectLocalDelivery();
+      $announce->addRemote($conf['endpoint_url']);
+  }
+
+  // A remote delivery device is a special device that delivers outgoing commands
+  // to remote endpoints
+  $remote = new Device\RemoteDeliveryDevice($remoteQueue, $deviceMap);
+  $remote->setDefaultEndpoint($conf['default_endpoint']);
+  $broker->addDevice($remote); // Add the remote delivery device
+
+  // Register defined devices
+  foreach($conf['devices'] as $d) {
+
+      $dn = $d->getDeviceName();
+
+      if(is_array($args['rundevices']) && count($args['rundevices']) > 0 && !in_array($dn, $args['rundevices'])) {
+          echo "Skip device ".$dn;
+          continue;
+      }
+
+      echo "Added device $dn\n";
+      $broker->addDevice($d);
+  }
+
+  $broker->run();
 }
 
-// ** DISABLE DIRECT LOCAL DELIVERY **
-// Direct local delivery can be disabled, in which case the message broker
-// will push all messages via the remote queue. We need to announce to ourself
-// so that those messages can be routed into our own endpoint.
-if($conf['disable-direct-local']) {
-    $broker->disableDirectLocalDelivery();
-    $announce->addRemote($conf['endpoint_url']);
-}
-
-// A remote delivery device is a special device that delivers outgoing commands
-// to remote endpoints
-$remote = new Device\RemoteDeliveryDevice($remoteQueue, $deviceMap);
-$remote->setDefaultEndpoint($conf['default_endpoint']);
-$broker->addDevice($remote); // Add the remote delivery device
-
-// Register defined devices
-foreach($conf['devices'] as $d) {
-
-    $dn = $d->getDeviceName();
-
-    if(is_array($args['rundevices']) && count($args['rundevices']) > 0 && !in_array($dn, $args['rundevices'])) {
-        echo "Skip device ".$dn;
-        continue;
-    }
-
-    echo "Added device $dn\n";
-    $broker->addDevice($d);
-}
-
-$broker->run();
+startBroker($conf);
