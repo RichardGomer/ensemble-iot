2b10657a1a90399fd1e235c65adfaeed8f50903d
Richard Gomer <richard@xebre.net>
2022-04-01 15:33:21 +0100

    Softstart test scripts

---
+++ mod/Irrigation/softstart/test.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../../../vendor/autoload.php';
+
+$t = new Ensemble\System\Thread("python3 -u ".dirname(__FILE__)."/softstart.py 40 40");
+
+echo "Begin";
+
+for($i = 0; $i < 3; $i++) {
+    sleep(1);
+    var_dump($t->read());
+}
+
+$t->close(2); // 2 = SIGINT
+
+while($t->isRunning()) {
+    echo "Waiting...";
+    usleep(250000);
+}
+
---
+++ mod/Irrigation/softstart/testlow.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Testing irrigation control"
+
+for valve in 23 21 26 #0 1 2 3 23 21 26 # 1 2 3 4 h1 h2 h3
+do
+	echo "Test valve $valve"
+
+	# Open a valve
+	gpio mode $valve out
+	gpio write $valve 0
+	echo "Valve ON"
+	sleep 0.2
+
+	# Test the pump
+	echo "Pumping"
+	echo "YOU MUST STOP PUMPING WITH CTRL-C"
+	python3 softstart.py 40 20 # physpin intensity%
+	echo "Pumping complete"
+	sleep 0.2
+
+	# Close the valve
+	gpio write $valve 1
+	echo "Valve OFF"
+	sleep 0.2
+done
+
+echo "Tests complete"
