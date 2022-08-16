a42ad426b7a1d2aa7f3995ae8792babd80dd12cd
Richard Gomer <richard@richardgomer.co.uk>
2022-08-16 21:07:34 +0100

    Update electricity tariff in OctopusGoTariffDevice :'( #fuckTheTories

--- mod/Schedule/OctopusGoTariffDevice.php
+++ mod/Schedule/OctopusGoTariffDevice.php
@@ -20,9 +20,20 @@ class OctopusGoTariffDevice extends SchedulerDevice {
 
         $midnight = strtotime('today midnight');
 
+        // Handle 2022 tariff change.
+        // 39.84p! What a useless criminal bunch of cunts we have in charge of the country
+        // Fuck the useless greedy lying tories
+        if($midnight >= strtotime('2022-08-19 00:00:00')) {
+            $drate = 39.84;
+            $nrate = 7.5;
+        } else {
+            $drate = 15.59;
+            $nrate = 5.00;
+        }
+
         // Generate for today and tomorrow
         for($n = 0; $n < 96; $n++) {
-          $cost = $n % 48 >= 1 && $n % 48 <= 8 ? 5.00 : 15.59;
+          $cost = $n % 48 >= 1 && $n % 48 <= 8 ? $nrate : $drate;
           $time = $midnight + $n * 1800;
           $s->setPeriod($time, $time+1800, $cost, false);
         }
